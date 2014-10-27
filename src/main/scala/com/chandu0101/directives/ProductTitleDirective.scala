f25ddc96fbaaff8046e5ab944946e4602f6d157a
chandra sekhar kode <chandu_csu@yahoo.com>
2014-10-27 10:47:05 +0530

    testing directive

--- src/main/scala/com/chandu0101/directives/ProductTitleDirective.scala
+++ src/main/scala/com/chandu0101/directives/ProductTitleDirective.scala
@@ -1,6 +1,6 @@
 package com.chandu0101.directives
 
-import com.greencatsoft.angularjs.{AttributeDirective, IsolatedScope, TemplatedDirective}
+import com.greencatsoft.angularjs.{ElementDirective, IsolatedScope, TemplatedDirective}
 
 import scala.scalajs.js.annotation.JSExport
 
@@ -8,7 +8,7 @@ import scala.scalajs.js.annotation.JSExport
  * Created by chandrasekharkode on 10/27/14.
  */
 @JSExport
-object ProductTitleDirective extends AttributeDirective with TemplatedDirective with IsolatedScope {
+object ProductTitleDirective extends ElementDirective with TemplatedDirective with IsolatedScope {
   override val templateUrl: String = "product-title.html"
   override val name = "productTitle"
 }
